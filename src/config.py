fd0ecc9775c6aec8682e811026ef3d7be9be5df1
Logey <xlogey@outlook.com>
2022-05-31 00:14:52 +0100

    exclude "bootleg" from song search

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 # ignore videos including these strings (not case sensitive)
-EXCLUSIONS = ["karaoke", "nightcore", "live", "remix", "8d audio", "clean"]
+# if the song or artist name includes this string, it will not be ignored
+EXCLUSIONS = ["karaoke", "nightcore", "live", "remix", "8d audio", "clean", "bootleg"]
 
 # maximum distance to accept if the song name is not found in the video title
 # used for typos - higher numbers are less strict

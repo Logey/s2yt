08d818ee85839b0d625f040fed36770d65cb423f
Logey <xlogey@outlook.com>
2022-05-31 09:28:45 +0100

    added more exclusion terms

--- src/config.py
+++ src/config.py
@@ -1,6 +1,24 @@
 # ignore videos including these strings (not case sensitive)
 # if the song or artist name includes this string, it will not be ignored
-EXCLUSIONS = ["karaoke", "nightcore", "live", "remix", "8d audio", "clean", "bootleg"]
+EXCLUSIONS = [
+  # no lyrics / instrumental version
+  "karaoke", "instrumental", "acoustic",
+
+  # unintended remixes
+  "nightcore", "remix", "bootleg",
+
+  # fan-edits / filters
+  "8d audio", "bass boost",
+
+  # censored versions
+  "clean",
+
+  # covers
+  "cover",
+
+  # live / concert recordings
+  "live", "concert", "austin city limits"
+]
 
 # maximum distance to accept if the song name is not found in the video title
 # used for typos - higher numbers are less strict

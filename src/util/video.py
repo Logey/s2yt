55e7fbd02a596fd7ed486ca1da1d08d403621650
Logey <xlogey@outlook.com>
2022-05-31 15:06:24 +0100

    try/except for video length in case its not found

--- src/util/video.py
+++ src/util/video.py
@@ -1,18 +1,21 @@
 def videoLengthToMS(length):
-  lengthSplit = length.split(":")
-  h = m = s = ms = 0
+  try:
+    lengthSplit = length.split(":")
+    h = m = s = ms = 0
 
-  if len(lengthSplit) == 2:
-    m = int(lengthSplit[0])
-    s = int(lengthSplit[1])
-  elif len(lengthSplit) == 3:
-    h = int(lengthSplit[0])
-    m = int(lengthSplit[1])
-    s = int(lengthSplit[2])
+    if len(lengthSplit) == 2:
+      m = int(lengthSplit[0])
+      s = int(lengthSplit[1])
+    elif len(lengthSplit) == 3:
+      h = int(lengthSplit[0])
+      m = int(lengthSplit[1])
+      s = int(lengthSplit[2])
 
-  if s > 0: ms += s*1000
-  if m > 0: ms += m*60*1000
-  if h > 0: ms += h*60*60*1000
+    if s > 0: ms += s*1000
+    if m > 0: ms += m*60*1000
+    if h > 0: ms += h*60*60*1000
+  except:
+    ms = 0
 
   return ms
 
